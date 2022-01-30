98af002a414f08f9c0aa88e6776d7dca03a5a4b9
jerryBak <parkchaewon8566@gmail.com>
2022-01-30 09:58:23 +0900

    level1_두개뽑아서더하기

--- programmers/level1_20.py
+++ programmers/level1_20.py
@@ -1,7 +1,3 @@
 def solution(numbers):
-    # for i in range(0,len(numbers)-1):
-    #     for j in range(i+1,len(numbers)):
-    #         answer.append(numbers[i]+numbers[j])
-    # answer=sorted(set(answer))
     answer = [numbers[i]+numbers[j] for i in range(0, len(numbers)-1) for j in range(i+1, len(numbers))]
     return sorted(list(set(answer)))
